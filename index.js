a38f8e8e2981ca6f168b70153d00f281285c38d3
Luke <luke.wangai@student.moringaschool.com>
2025-03-09 14:14:56 +0300

    The Math Lab with Js

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2 
 
-// Write your code here
+
+
+
+
+let random = 
+
+    (Math.floor ((Math. random()*10)+1))
+
+
+
+
+let num3 = 76
+let num4 = 9
+
+const mod = num3 % num4
+
+
+let max =
+Math.max(1, 2, 3, 4, 5, 6, 7, 20)
+
+  
+
+
